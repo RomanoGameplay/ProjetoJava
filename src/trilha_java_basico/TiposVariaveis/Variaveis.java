cde7fbe65ff77ae2b40eb56d8e26fbad8a504a49
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2022-11-09 10:52:08 -0300

    Modificacao da classe Variaveis

--- src/trilha_java_basico/TiposVariaveis/Variaveis.java
+++ src/trilha_java_basico/TiposVariaveis/Variaveis.java
@@ -2,6 +2,10 @@ package trilha_java_basico.TiposVariaveis;
 
 public class Variaveis {
     public static void main(String[] args) {
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
 
+        final double VALOR_PI = 3.14;
+        System.out.println(VALOR_PI);
     }
 }
