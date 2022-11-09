2b0db8b0d5e65940e1a70aa86a9378f1900f84c0
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2022-11-09 11:18:31 -0300

    Modificações na classe Operadores.java

--- src/trilha_java_basico/Operadores/Operadores.java
+++ src/trilha_java_basico/Operadores/Operadores.java
@@ -2,6 +2,23 @@ package trilha_java_basico.Operadores;
 
 public class Operadores {
     public static void main(String[] args) {
+        System.out.println("LINGUAGEM"+"JAVA");
 
+        int num = 5;
+        num = - num;
+        System.out.println(num);
+        num = - num;
+        System.out.println(num);
+
+        num = 7;
+//        num++;
+//        System.out.println(num+2);
+        System.out.println(num);
+        System.out.println(++num);
+//        System.out.println(num);
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
     }
 }
