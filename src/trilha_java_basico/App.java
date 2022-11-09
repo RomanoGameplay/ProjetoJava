17ae3c368755dac3b4c9da8ae3bbc94af87a7611
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2022-11-09 08:58:31 -0300

    Criação do método nomeCompleto

--- src/trilha_java_basico/App.java
+++ src/trilha_java_basico/App.java
@@ -3,5 +3,14 @@ package trilha_java_basico;
 public class App {
     public static void main(String [] args){
         System.out.println("olá, mundo!");
+        String primeiroNome = "Caio";
+        String segundoNome = "Sampaio";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
