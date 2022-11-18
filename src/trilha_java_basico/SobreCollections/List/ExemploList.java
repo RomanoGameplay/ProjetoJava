57ef27d6fc7ab5ed065e1ff5a84c01dba2a22a96
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2022-11-18 07:18:33 -0300

    Término da classe ExemploList e criação da classe ExemploOrdenacaoList

--- src/trilha_java_basico/SobreCollections/List/ExemploList.java
+++ src/trilha_java_basico/SobreCollections/List/ExemploList.java
@@ -2,6 +2,8 @@ package trilha_java_basico.SobreCollections.List;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class ExemploList {
@@ -14,18 +16,55 @@ public class ExemploList {
         notas.add(7.0);
         notas.add(0.0);
         notas.add(3.6);
-        System.out.println(notas.toString());
+        System.out.println(notas);
 
         System.out.println(notas.indexOf(5.d));
         notas.add(4, 8.0);
-        System.out.println(notas.toString());
+        System.out.println(notas);
         notas.set(notas.indexOf(5d), 6.0);
-        System.out.println(notas.toString());
+        System.out.println(notas);
 
         System.out.println("Está na lista: "+ notas.contains(5d));
         System.out.println("Está na lista: "+ notas.contains(6d));
 
         for (double nota: notas) {System.out.println(nota);}
+
+        System.out.println(notas.get(2));
+        System.out.println("Menor nota: "+ Collections.min(notas));
+        System.out.println("Maior nota: "+ Collections.max(notas));
+
+        System.out.println("Soma dos valores: ");
+        Iterator<Double> it = notas.iterator();
+        Double soma = 0d;
+        while (it.hasNext()){
+            Double next = it.next();
+            soma += next;
+        }
+        System.out.println(soma);
+
+        System.out.println("Média: "+(soma/notas.size()));
+        System.out.println("Remover a nota zero: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remover na posição 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Notas menores que 7: ");
+
+        Iterator<Double> it1 = notas.iterator();
+        while (it1.hasNext()){
+            Double next = it1.next();
+            if (next < 7) it1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apagar toda a lista: ");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("Lista vazia: "+notas.isEmpty());
     }
 
 }
---
+++ src/trilha_java_basico/SobreCollections/List/ExemploOrdenacaoList.java
@@ -0,0 +1,8 @@
+package trilha_java_basico.SobreCollections.List;
+
+
+public class ExemploOrdenacaoList {
+    public static void main(String[] args) {
+        
+    }
+}
