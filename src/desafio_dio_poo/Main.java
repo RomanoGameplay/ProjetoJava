cf48c29fc3a0d2fa3cd3d6f34adecf70563f7e28
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2022-11-22 07:08:34 -0300

    renovacao

---
+++ src/desafio_dio_poo/Main.java
@@ -0,0 +1,30 @@
+package desafio_dio_poo;
+
+import desafio_dio_poo.dominio.Curso;
+import desafio_dio_poo.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
